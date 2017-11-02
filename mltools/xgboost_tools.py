983ffbcda1fa05bd5bd9c5a553d29c1ebec92553
Aleksandr Loskutov <aloskutyan@gmail.com>
2017-11-02 19:02:59 +0300

    unified XGBoost optmizer

--- mltools/xgboost_tools.py
+++ mltools/xgboost_tools.py
@@ -51,50 +51,48 @@ class XGBoostParamsOptimizer:
         return {'loss' : score, 'status': STATUS_OK}
     
     def _optimize_trees(self, trials, max_evals, max_depth_min, max_depth_max, min_eta, max_eta, eta_step,
-                        objective, score_metric, extra_metrics, zero_min_child_weight, zero_gamma,
-                        use_hists, grow_policy, max_numround, scale_pos_weight, num_class):
+                        min_lambda, max_lambda, min_alpha, max_alpha, zero_alpha, default_lambda,
+                        objectives, score_metric, extra_metrics, zero_min_child_weight, zero_gamma,
+                        max_numround, scale_pos_weight, num_class):
         
         gamma = 0.0 if zero_gamma else hp.qloguniform('gamma', np.log(1e-5), np.log(1e1), 1)
         min_child_weight = 0.0 if zero_min_child_weight else hp.qloguniform('min_child_weight',
                                                                             np.log(1e-5), np.log(1e1), 1)
+        lamb = 1.0 if default_lambda else hp.qloguniform('lambda', np.log(min_lambda), np.log(max_lambda), 1)
+        alpha = 0.0 if zero_alpha else hp.qloguniform('alpha', np.log(min_alpha), np.log(max_alpha), 1)
         space = {
             'eta' : hp.quniform('eta', min_eta, max_eta, eta_step),
             'max_depth' : hp.quniform('max_depth', max_depth_min, max_depth_max, 1),
             'min_child_weight' : min_child_weight,
             'subsample' : hp.quniform('subsample', 0.5, 1, 0.05),
             'colsample_bytree' : hp.quniform('colsample_bytree', 0.5, 1, 0.05),
-            'objective': objective,
+            'objective': hp.choice('objective', objectives),
             'gamma' : gamma,
+            'alpha' : alpha,
+            'lambda' : lamb,
             'silent' : 1,
             'score_metric' : score_metric,
             'extra_metrics' : extra_metrics,
             'numround' : max_numround,
         }
-        if objective.startswith('binary'):
+        if objectives[0].startswith('binary'):
             space['scale_pos_weight'] = scale_pos_weight
-        if objective.startswith('multi'):
+        if objectives[0].startswith('multi'):
             space['num_class'] = num_class
-        if use_hists:
-            space['tree_method'] = 'hist'
-            space['grow_policy'] = grow_policy
 
         best = fmin(self._xgb_binary_score, space, algo=tpe.suggest, trials=trials, max_evals=max_evals)
         print(best)
         
-    def get_optimal_xgb_params(self, features, target, folds, binary=True, score_metric=None, extra_metrics=None,
-                               max_depth_min=2, max_depth_max=6, min_eta=0.02, max_eta=0.2, eta_step=0.02,
-                               max_evals=50, zero_min_child_weight=True, zero_gamma=True, use_hists=False,
-                               grow_policy='depthwise', weigh_positives=False, max_numround=1000,
-                               task_name='xgb_params', tmp_dir=None, remove_tmp_dir=False):
-        if score_metric is not None:
-            if binary and score_metric not in ['logloss', 'error', 'rmse', 'auc']:
-                raise ValueError('use correct metrics for binary tasks')
-            if not binary and score_metric not in ['mlogloss', 'merror']:
-                raise ValueError('use correct metrics for multiclass tasks')
-        if score_metric is None:
-            score_metric = 'logloss' if binary else 'mlogloss'
-        objective = 'binary:logistic' if binary else 'multi:softprob'
-        num_class = None if binary else len(target.value_counts())
+    def get_optimal_exact_xgb_params(self, features, target, folds, objectives, score_metric, extra_metrics=[],
+                                     max_depth_min=2, max_depth_max=6, min_eta=0.02, max_eta=0.2, eta_step=0.02,
+                                     min_lambda=1.0, max_lambda=1.0, min_alpha=1.0, max_alpha=1.0,
+                                     max_evals=50, zero_min_child_weight=True, zero_gamma=True, zero_alpha=True,
+                                     default_lambda=True, weigh_positives=False, max_numround=1000,
+                                     task_name='xgb_params', tmp_dir=None, remove_tmp_dir=True):
+        if isinstance(objectives, str):
+            objectives = [objectives]
+        
+        num_class = len(target.value_counts()) if objectives[0].startswith('multi') else None
         positive_number = np.sum(target)
         scale_pos_weight = (len(target) - positive_number) / positive_number if weigh_positives else 1.0
         
@@ -118,8 +116,9 @@ class XGBoostParamsOptimizer:
 
         trials = Trials()
         self._optimize_trees(trials, max_evals, max_depth_min, max_depth_max, min_eta, max_eta, eta_step, 
-                             objective, score_metric, extra_metrics, zero_min_child_weight, zero_gamma,
-                             use_hists, grow_policy, max_numround, scale_pos_weight, num_class)
+                             min_lambda, max_lambda, min_alpha, max_alpha, zero_alpha, default_lambda,
+                             objectives, score_metric, extra_metrics, zero_min_child_weight, zero_gamma,
+                             max_numround, scale_pos_weight, num_class)
         
         if remove_tmp_dir:
             shutil.rmtree(self._tmp_dir)
