7645dcd31285a33e55c8107f32f72fea6928e0fd
Aleksandr Loskutov <aloskutyan@gmail.com>
2017-12-07 18:35:25 +0300

    1.30: utilities added

---
+++ mltools/utilities.py
@@ -0,0 +1,8 @@
+import multiprocessing
+
+def map_async(f, tasks, num_processes=None):
+    p = multiprocessing.Pool(processes=num_processes)
+    r = p.map_async(f, tasks)
+    p.close()
+    p.join()
+    return r.get()
