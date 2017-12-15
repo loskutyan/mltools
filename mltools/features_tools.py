0aa17e317a4949e4e5fbb72b2dfd18fed0752ac3
Aleksandr Loskutov <aloskutyan@gmail.com>
2017-12-15 19:04:47 +0300

    1.31: fio and geo replacement

--- mltools/features_tools.py
+++ mltools/features_tools.py
@@ -173,8 +173,11 @@ class Tokenizer:
         self.stopwords = []
         self.token_filter = lambda x: x[3] is not None
         self.concat_no_s = True
-        self.drop_fio_and_geo = True
-        self.fio_and_geo_parts = ['имя', 'фам', 'отч', 'гео']
+        self.replace_fio_and_geo = True
+        self.fio_and_geo_parts = {'имя' : 'имяимя',
+                                  'фам' : 'фамфам',
+                                  'отч' : 'отчотч',
+                                  'гео' : 'геогео'}
 
     def get_token(self, item):
         if 'analysis' in item.keys() and len(item['analysis']) > 0:
@@ -247,8 +250,11 @@ class Tokenizer:
                 english_collector.append(token[0])
                 continue
 
-            if self.drop_fio_and_geo and token[3] is not None and any([x in token[3] for x in self.fio_and_geo_parts]):
-                continue
+            fio_and_geo_replacements = {x : self.fio_and_geo_parts[x] for x in token[3]
+                                        if x in self.fio_and_geo_parts}
+                
+            if self.replace_fio_and_geo and token[3] is not None and len(fio_and_geo_replacements) > 0:
+                token[0] = list(fio_and_geo_replacements.values())[0]
             
             if token[0] in self.whitelist or (token[0] not in self.stopwords and self.token_filter(token)):
                 add_to_sentence(token, sentence)
@@ -810,3 +816,4 @@ class ClassUniqueTokensFeaturesCalculator:
                  strongly_unique_cl_tokens_number, strongly_unique_all_tokens_number,
                  min_cl_token_freq, appear, count, lowercase)
         return self.transform(tokens_lists.map(lambda x: ' '.join(x)))
+
